160916c0e1d9a00dbd5947efa6dd1fe8b17497a5
julian-heng <julianhengwl@gmail.com>
2021-04-11 14:45:54 +0800

    Update open_item2.sh

--- scripts/open_iterm2.sh
+++ scripts/open_iterm2.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 # Detects if iTerm2 is running
-if ! pgrep -f "iTerm" > /dev/null; then
+if ! pgrep -f "iTerm" > /dev/null 2>&1; then
     open -a "/Applications/iTerm.app"
 else
     # Create a new window
-    if ! osascript -e 'tell application "iTerm2" to create window with default profile' > /dev/null; then
+    script='tell application "iTerm2" to create window with default profile'
+    ! osascript -e "${script}" > /dev/null 2>&1 && {
         # Get pids for any app with "iTerm" and kill
-        for i in $(pgrep -f "iTerm"); do kill -15 "$i"; done
-        open  -a "/Applications/iTerm.app"
-    fi
+        while IFS="" read -r pid; do
+            kill -15 "${pid}"
+        done < <(pgrep -f "iTerm")
+        open -a "/Applications/iTerm.app"
+    }
 fi
